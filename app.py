a71d832bc9a686b40c9171564c0f21bf6c40c6de
Mutai05 <kelvok2014@gmail.com>
2024-03-24 07:59:36 +0300

    Sqlite Database setup

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 # Create a Flask application instance
 app = Flask(__name__)
+app.config[SQLALCHEMY_DATABASE_URI] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+
 
 # Define a route for the Homepage
 @app.route('/')
