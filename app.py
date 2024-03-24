6acb72ef83fb2b127bee194a1b10e94416676ce9
Mutai05 <kelvok2014@gmail.com>
2024-03-24 09:34:29 +0300

    Initial commit

--- app.py
+++ app.py
@@ -1,18 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # Create a Flask application instance
 app = Flask(__name__)
-app.config[SQLALCHEMY_DATABASE_URI] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 # Define a route for the Homepage
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+# Create the database tables (inside application context)
+with app.app_context():
+    db.create_all()
 
 # Run the Flask Application
 if __name__ == "__main__":
